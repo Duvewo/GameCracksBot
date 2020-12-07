5cc14dc448ceec8ae45fc7c52b570303a7093749
Duvewo <dkarimov03@gmail.com>
2020-12-07 18:08:42 +0500

    Added command to start

--- commands/start.go
+++ commands/start.go
@@ -4,6 +4,6 @@ import tb "gopkg.in/tucnak/telebot.v2"
 
 func OnStart(b *tb.Bot) func(m *tb.Message){
 	return func(m *tb.Message) {
-		b.Send(m.Sender, "Hello!\nAvailable commands:\n/cracks - Show to you latest cracks")
+		b.Send(m.Sender, "Hello!\nAvailable commands:\n/cracks - Show to you latest cracks\n/games - Search games by filter")
 	}
 }
